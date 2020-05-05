eb70d1f6caf6f7142622457210363e1f59a67344
miguel jimenez <denkatalabeno@gmail.com>
2020-05-05 20:10:19 +0800

    finished the exercise

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log("I am connected!");
+
+
+// Select the text that I have on the input - done
+// Select the button - done
+
+const inputPoke = document.querySelector(".form-control");
+const searchButton = document.querySelector(".btn");
+const abilities = document.querySelector(".abilities");
+// Add an event listener to the button
+// In the moment I click
+
+searchButton.addEventListener('click', () => {
+  // Do Magic!
+  let pokemon = inputPoke.value;
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+  .then(res => res.json()
+    .then(data => {
+      abilities.innerHTML = '';
+      data.abilities.forEach(ability => {
+        abilities.innerHTML += `<li> ${ability.ability.name} </li>`
+      })
+      // Do more magic!
+      // I need to find an element where I can insert the abilities
+
+      }
+    ))
+  })
+  // When I get the information back
+  // Show the abilities into the website
